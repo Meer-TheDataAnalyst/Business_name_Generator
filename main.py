75e8010e8823152904817c9fe9773c5b7f4984c6
Meer-TheDataAnalyst <136861720+Meer-TheDataAnalyst@users.noreply.github.com>
2023-07-01 18:16:22 +0530

    Add files via upload
    # star with greetings
    print("welcome to the Business name genrator")
    #name the city you grew up in?
    city= input("whats the name of the city you grew up in? ")
    #name the business category you are looking to start?
    Category= input("name the Category of business you want to start?\n")
    #combine the name of the city and the category of business.
    business_name =city+" "+Category
    #now print the busiess name.
    print("The name of your Business is :  " + business_name)

---
+++ main.py
@@ -0,0 +1,10 @@
+# star with greetings
+print("welcome to the Business name genrator")
+#name the city you grew up in?
+city= input("whats the name of the city you grew up in? ")
+#name the business category you are looking to start?
+Category= input("name the Category of business you want to start?\n")
+#combine the name of the city and the category of business.
+business_name =city+" "+Category
+#now print the busiess name.
+print("The name of your Business is :  " + business_name)
